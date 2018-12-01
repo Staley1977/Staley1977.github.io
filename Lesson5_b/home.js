7fbcb4ba3a8dc4cd6ffb5f5470aeaf4006348e2d
unknown <damien.cheong@yahoo.com>
2018-12-01 16:21:46 +0800

    Lesson5b

---
+++ Lesson5_b/home.js
@@ -0,0 +1,43 @@
+var my_name;
+var my_age;
+var my_birthdate;
+
+my_name="Stackup";
+my_age=23;
+my_birthdate="20/03/1988"
+
+console.log(my_name);
+console.log(my_age);
+console.log(my_birthdate);
+
+var my_address = "JTC Launchpad";
+
+var my_test;
+
+console.log(typeof(my_name));
+console.log(typeof(my_test));
+
+var firstName;
+var lastName;
+
+firstName="Nick";
+lastName="Test";
+
+var fullName=firstName+lastName;
+
+console.log(typeof(fullName));
+console.log(fullName);
+
+var space;
+space=" ";
+var fullerName=firstName+space+lastName;
+
+console.log(fullerName);
+
+var fullerName=firstName+" "+lastName;
+
+var qty=2;
+var total=200;
+var qtyTotal = qty+total;
+
+console.log(qtyTotal);
